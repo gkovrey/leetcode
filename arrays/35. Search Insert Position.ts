8083cdaa165e75ef260db654b3039d0fdb8e44dc
georgiy.kovrey <georgiy.kovrey@skywindgroup.com>
2022-10-08 19:04:10 +0200

    problem/35 Save old algo.

--- arrays/35. Search Insert Position.ts
+++ arrays/35. Search Insert Position.ts
@@ -1,19 +1,23 @@
 function searchInsert(nums: number[], target: number): number {
-
     if (nums.length == 0) return 0;
-    if (nums.length == 1) {
-        if (nums[0] < target ) return 1;
-        if (nums[0] == target ) return 0;
-        if (nums[0] > target) return 0;
-    }
-    const middle = Math.floor((nums.length)/2);
-    const average = nums[middle];
-    if (target == average) return middle;
-    if (target > average) {
-        return middle + searchInsert(nums.slice(middle + 1, nums.length), target)
-    } else {
-        if (middle > 1) return searchInsert(nums.slice(0, middle - 1), target)
-        return searchInsert(nums.slice(0, middle), target)
+    if (target <= (nums?.at(0) || 0)) return 0;
+    if (target > (nums?.at(-1) || 0)) return nums.length;
+
+    let middle = Math.floor((nums.length)/2);
+    while (true) {
+        let average = nums.at(middle) || 0;
+        if (target == average) return middle;
+
+        if (target > average && target < (nums.at(middle + 1) || 0)) return middle + 1;
+        if (target <= average && target > (nums.at(middle - 1) || 0)) return middle;
+        if (target > average) {
+            middle = Math.round(middle + (nums.length - middle) / 2);
+        } else {
+            middle = Math.floor(middle / 2);
+        }
+
+        if (middle >= nums.length) return nums.length;
+        if (middle == 0 ) return  0;
     }
 
 };
@@ -26,6 +30,12 @@ console.log(searchInsert([1,3,5,6],0)); // 0
 console.log(searchInsert([1],0)); // 0
 console.log(searchInsert([1,3],2)); // 1
 console.log(searchInsert([1,2,4,6,7], 3)); // 2
+console.log(searchInsert([1], 2)); // 1
+console.log(searchInsert([1], 1)); // 0
+console.log(searchInsert([2,4,5,6,7,8], 7)); // 4
+console.log(searchInsert([1,3,4,5,6,8,10], 5)); // 3
+
+
 
 
 
